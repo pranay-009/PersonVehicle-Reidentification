af1db2b45dc5b890b539c569a77410d23d41f293
Pranay Saha <pranaysaha@Pranays-MacBook-Air.local>
2024-01-05 01:40:00 +0530

    removed main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.segmentation.inference import Inference
-import os
-
-
-
-inf = Inference(os.path.join(os.getcwd(),"output/weights/res_humanid_model.keras"),os.path.join(os.getcwd(),"output/download.png"))
-a,b,img= inf.predictions(os.path.join(os.getcwd(),"output/121.jpg"))
-inf.plot(a,b,img)
